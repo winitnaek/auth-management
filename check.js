81a072ea775bcf9874bf72d28c1b04f898e2d8fe
Igor Vernik <igorV@bsihq.com>
2019-06-10 12:41:54 -0400

    Updated redirect to login after logout steps.

--- check.js
+++ check.js
@@ -1,24 +1,28 @@
 var userDataUp = sessionStorage.getItem('up');
-var userdata   = JSON.parse(userDataUp);
-if(!userdata){
-    window.location="login.html?user=nouser";
+var userdata = JSON.parse(userDataUp);
+if (!userdata) {
+  window.location = "login.html?user=nouser";
 }
 const macLogin = (function () {
-    function logout() {
-      sessionStorage.removeItem(macLogin.appdata.userkey);
-      fetch('/sws/r/webapi/logout',{
-        credentials: 'same-origin'
-      }).then(function (resp) {        
-          window.location = "mgmtui_login";        
-      });
+  function logout() {
+    sessionStorage.removeItem(macLogin.appdata.userkey);
+    fetch('/sws/r/webapi/logout', {
+      credentials: 'same-origin'
+    }).then(function (resp) {
+      console.log("Logout Success: " + JSON.stringify(resp));
+      window.location = "login.html?user=nouser";
+    }).catch(function(error) {
+      console.log('There has been a problem with "logout": ', error.message);
+      window.location = "login.html?user=nouser";
+    });
+  }
+  return {
+    appdata: {
+      userkey: 'up'
+    },
+
+    doLogout: function (form) {
+      logout();
     }
-    return {
-      appdata: {
-        userkey:'up'
-      },
-      
-      doLogout: function (form) {
-        logout();
-      }
-    };
-  })();
+  };
+})();
