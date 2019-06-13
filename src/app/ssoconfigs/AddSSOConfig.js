13e59fe61b8cb59a3dc74f73bc052bff8f0ebe54
Vinit Naik <vnaik@bsihq.com>
2019-06-13 14:11:57 -0400

    Removed some fields and added validation for singnature

--- src/app/ssoconfigs/AddSSOConfig.js
+++ src/app/ssoconfigs/AddSSOConfig.js
@@ -53,13 +53,10 @@ class AddSSOConfig extends React.Component {
         this.validateSSOConfig    = this.validateSSOConfig.bind(this);
         this.dsplNameChange       = this.dsplNameChange.bind(this);
         this.idpIssuerChange      = this.idpIssuerChange.bind(this);
-        this.idpReqURLChange      = this.idpReqURLChange.bind(this);
-        this.spIssuerChange       = this.spIssuerChange.bind(this);
         this.certTextChanged      = this.certTextChanged.bind(this);
-        this.certPasswordChanged  = this.certPasswordChanged.bind(this);
         this.certAliasChanged     = this.certAliasChanged.bind(this);
-        this.appRedirectUrlChanged= this.appRedirectUrlChanged.bind(this);
         this.validateCert         = this.validateCert.bind(this);
+        this.validateRespSignatureChanged=this.validateRespSignatureChanged.bind(this);
         
     }
    
@@ -73,24 +70,25 @@ class AddSSOConfig extends React.Component {
     }
     componentDidMount(){
         if(!this.state.modeisnew){
-            const {appRedirectURL,nonSamlLogoutURL, enabled, dsplName, idpIssuer, idpReqURL, spConsumerURL, spIssuer, attribIndex, validateRespSignature, validateIdpIssuer, allowLogout,redirectToApplication,expireRequestSecs,certAlias,certPassword,certText} = this.state.rowdata;
+            const {enabled, dsplName, idpIssuer, validateRespSignature, allowLogout,expireRequestSecs,certAlias,certText} = this.state.rowdata;
             this.dsplName.value=dsplName;
             this.idpIssuer.value=idpIssuer;
-            this.idpReqURL.value = idpReqURL;
-            this.spConsumerURL.value= spConsumerURL;
-            this.spIssuer.value= spIssuer;
-            this.attribIndex.value= attribIndex;
-            this.validateRespSignature.checked= validateRespSignature;
-            this.validateIdpIssuer.checked= validateIdpIssuer;
             this.allowLogout.checked =allowLogout;
-            this.redirectToApplication.checked =redirectToApplication;
-            this.nonSamlLogoutURL.value=nonSamlLogoutURL;
-            this.appRedirectURL.value=appRedirectURL;
+            this.validateRespSignature.checked= validateRespSignature;
             this.certAlias.value=certAlias;
-            this.certPassword.value=certPassword;
             this.certText.value=certText;
+            if(validateRespSignature){
+                this.certText.disabled=false;
+                this.certAlias.disabled=false;
+            }else{
+                this.certText.disabled=true;
+                this.certAlias.disabled=true;
+            }
             this.expireRequestSecs.value= expireRequestSecs;
             this.enabled.checked =enabled;
+        }else{
+            this.certText.disabled=true;
+            this.certAlias.disabled=true;
         }
     } 
     toggleUIConfirmSave() {
@@ -102,18 +100,9 @@ class AddSSOConfig extends React.Component {
                     "acctName": this.state.selectedAccount.label,
                     "dsplName": this.dsplName.value,
                     "idpIssuer":this.idpIssuer.value,
-                    "idpReqURL":this.idpReqURL.value,
-                    "spConsumerURL":this.spConsumerURL.value,
-                    "spIssuer":this.spIssuer.value,
-                    "attribIndex":this.attribIndex.value,
                     "validateRespSignature":this.validateRespSignature.checked,
-                    "validateIdpIssuer":this.validateIdpIssuer.checked,
                     "allowLogout":this.allowLogout.checked,
-                    "redirectToApplication":this.redirectToApplication.checked,
-                    "nonSamlLogoutURL":this.nonSamlLogoutURL.value,
-                    "appRedirectURL": this.appRedirectURL.value,
                     "certAlias":this.certAlias.value,
-                    "certPassword":this.certPassword.value,
                     "certText":this.certText.value,
                     "expireRequestSecs":this.expireRequestSecs.value,
                     "enabled": this.enabled.checked 
@@ -133,18 +122,9 @@ class AddSSOConfig extends React.Component {
                     "acctName": this.state.selectedAccount.label,
                     "dsplName": this.dsplName.value,
                     "idpIssuer":this.idpIssuer.value,
-                    "idpReqURL":this.idpReqURL.value,
-                    "spConsumerURL":this.spConsumerURL.value,
-                    "spIssuer":this.spIssuer.value,
-                    "attribIndex":this.attribIndex.value,
                     "validateRespSignature":this.validateRespSignature.checked,
-                    "validateIdpIssuer":this.validateIdpIssuer.checked,
                     "allowLogout":this.allowLogout.checked,
-                    "redirectToApplication":this.redirectToApplication.checked,
-                    "nonSamlLogoutURL":this.nonSamlLogoutURL.value,
-                    "appRedirectURL": this.appRedirectURL.value,
                     "certAlias":this.certAlias.value,
-                    "certPassword":this.certPassword.value,
                     "certText":this.certText.value,
                     "expireRequestSecs":this.expireRequestSecs.value,
                     "enabled": this.enabled.checked 
@@ -163,10 +143,10 @@ class AddSSOConfig extends React.Component {
     }
     validateSSOConfig(){
         let isValidConfig = true;
-        var selectedEmp = this.state.selectedAccount;
-        console.log('selectedEmp');
-        console.log(selectedEmp);
-        if(!selectedEmp){
+        var selectedAcct = this.state.selectedAccount;
+        console.log('selectedAcct');
+        console.log(selectedAcct);
+        if(!selectedAcct){
             this.setState({selectreqstate:this.state.selectrequired,selectfeestate:this.state.selectfeedback});
             return isValidConfig = false;
         }else{
@@ -187,46 +167,18 @@ class AddSSOConfig extends React.Component {
             this.setState({idpIssuerstate:false});
             isValidConfig = true;
         }
-        if(!this.idpReqURL.value){
-            this.setState({idpReqURLstate:true});
-            return isValidConfig = false;
-        }else{
-            this.setState({idpReqURLstate:false});
-            isValidConfig = true;
-        }
-        if(!this.spIssuer.value){
-            this.setState({spIssuerstate:true});
-            return isValidConfig = false;
-        }else{
-            this.setState({spIssuerstate:false});
-            isValidConfig = true;
-        }
-        if(!this.appRedirectURL.value){
-            this.setState({appRedirectURLstate:true});
-            return isValidConfig = false;
-        }else{
-            this.setState({appRedirectURLstate:false});
-            isValidConfig = true;
-        }
-        if(!this.certAlias.value){
+        if(this.validateRespSignature.checked && !this.certAlias.value){
             this.setState({certAliasstate:true});
             return isValidConfig = false;
         }else{
             this.setState({certAliasstate:false});
             isValidConfig = true;
         }
-        if(!this.certPassword.value){
-            this.setState({certPasswordstate:true});
-            return isValidConfig = false;
-        }else{
-            this.setState({certPasswordstate:false});
-            isValidConfig = true;
-        }
-        if(!this.certText.value){
+        if(this.validateRespSignature.checked && !this.certText.value){
             this.setState({certTextstate:true,certErrorLable:'Required'});
             return isValidConfig = false;
         }else{
-            if(!this.validateCert()){
+            if(this.validateRespSignature.checked && !this.validateCert()){
                 this.setState({certTextstate:true,certErrorLable:'Invalid Certificate'});
                 console.log('Entered certificate PEM Text is invalid. >> Please provide valid certificate.');
                 return isValidConfig = false;
@@ -280,11 +232,14 @@ class AddSSOConfig extends React.Component {
     idpIssuerChange(e){
         this.validateSSOConfig();
     }
-    spIssuerChange(e){
-        this.validateSSOConfig();
-    }
-    idpReqURLChange(e){
-        this.validateSSOConfig();
+    validateRespSignatureChanged(){
+        if(this.validateRespSignature.checked){
+            this.certText.disabled=false;
+            this.certAlias.disabled=false;
+        }else{
+            this.certText.disabled=true;
+            this.certAlias.disabled=true;
+        }
     }
     certTextChanged(e){
         this.validateSSOConfig();
@@ -293,9 +248,6 @@ class AddSSOConfig extends React.Component {
         this.validateSSOConfig();
     }
     certAliasChanged(e){
-        this.validateSSOConfig();
-    }
-    appRedirectUrlChanged(e){
         this.validateSSOConfig();
     }
     toggleUIConfirmCancel() {
@@ -334,96 +286,44 @@ class AddSSOConfig extends React.Component {
                             </FormGroup>
                             <FormGroup row style={{marginBottom:8}}>
                                 <Label sm={1}></Label>
-                                <Label sm={1}>idpIssuer</Label>
+                                <Label sm={1}>Issuer</Label>
                                 <Col sm={4}>
-                                    <Input type="text"  invalid={this.state.idpIssuerstate} onChange={this.idpIssuerChange} name="idpIssuer" innerRef={(input) => this.idpIssuer = input}  placeholder="Enter idpIssuer" />
+                                    <Input type="text"  invalid={this.state.idpIssuerstate} onChange={this.idpIssuerChange} name="idpIssuer" innerRef={(input) => this.idpIssuer = input}  placeholder="Enter Issuer" />
                                     <FormFeedback invalid={this.state.idpIssuerstate}>Required</FormFeedback>
                                 </Col>
-                                <Label sm={1}>idpReqURL</Label>
-                                <Col sm={4}>
-                                    <Input type="text" name="idpReqURL" invalid={this.state.idpReqURLstate} onChange={this.idpReqURLChange} innerRef={(input) => this.idpReqURL = input}  id="idpReqURL" placeholder="Enter idpReqURL" />
-                                    <FormFeedback invalid={this.state.idpReqURLstate}>Required</FormFeedback>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row style={{marginBottom:8}}>
-                                <Label sm={1}></Label>
-                                <Label sm={1}>spConsumerURL</Label>
-                                <Col sm={4}>
-                                    <Input type="text" name="spConsumerURL" innerRef={(input) => this.spConsumerURL = input} placeholder="Enter spConsumerURL" />
-                                </Col>
-                                <Label sm={1}>spIssuer</Label>
-                                <Col sm={4}>
-                                    <Input type="text" name="spIssuer" invalid={this.state.spIssuerstate}  onChange={this.spIssuerChange}  id="spIssuer" innerRef={(input) => this.spIssuer = input} placeholder="Enter spIssuer" />
-                                    <FormFeedback invalid={this.state.spIssuerstate}>Required</FormFeedback>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row style={{marginBottom:8}}>
-                                <Label sm={1}></Label>
-                                <Label sm={1}>attribIndex</Label>
-                                <Col sm={4}>
-                                    <Input type="number" name="attribIndex"  innerRef={(input) => this.attribIndex = input} defaultValue={99}  placeholder="Enter attribIndex" />
-                                </Col>
-                               
-                            </FormGroup>
-                            <FormGroup row style={{marginBottom:8}}>
-                                <Label sm={1}></Label>
                                 <Label sm={1}></Label>
                                 <Col sm={2}>
-                                <CustomInput type="switch" innerRef={(input) => this.redirectToApplication = input} onChange={this.redirectToApplicationChanged} defaultChecked={redirectToApplication} name="redirectToApplication" id="redirectToApplication" label="redirectToApplication"/>
+                                <CustomInput type="switch" innerRef={(input) => this.allowLogout = input} onChange={this.allowLogoutChanged} defaultChecked={allowLogout} name="allowLogout" id="allowLogout" label="Allow Logout"/>
                                 </Col>
                                 <Col sm={2}>
-                                <CustomInput type="switch" innerRef={(input) => this.allowLogout = input} onChange={this.allowLogoutChanged} defaultChecked={allowLogout} name="allowLogout" id="allowLogout" label="allowLogout"/>
-                                </Col>
-                                <Label sm={1}></Label>
-                                <Col sm={2}>
-                                <CustomInput type="switch" innerRef={(input) => this.validateRespSignature = input} onChange={this.validateRespSignatureChanged} defaultChecked={validateRespSignature} id="validateRespSignature" name="validateRespSignature" label="validateRespSignature"/>
-                                </Col>
-                                <Col sm={2}>
-                                <CustomInput type="switch" innerRef={(input) => this.validateIdpIssuer = input} onChange={this.validateIdpIssuerChanged} defaultChecked={true} id="validateIdpIssuer" name="validateIdpIssuer" label="validateIdpIssuer"/>
+                                <CustomInput type="switch" innerRef={(input) => this.validateRespSignature = input} onChange={this.validateRespSignatureChanged} defaultChecked={validateRespSignature} id="validateRespSignature" name="validateRespSignature" label="Validate Signature"/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row style={{marginBottom:8}}>
                                 <Label sm={1}></Label>
-                                <Label sm={1}>nonSamlLogoutURL</Label>
+                                <Label sm={1}>Certificate Alias</Label>
                                 <Col sm={4}>
-                                    <Input type="text" name="nonSamlLogoutURL" innerRef={(input) => this.nonSamlLogoutURL = input} placeholder="Enter nonSamlLogoutURL" />
-                                </Col>
-                                <Label sm={1}>appRedirectURL</Label>
-                                <Col sm={4}>
-                                    <Input type="text" name="appRedirectURL"  invalid={this.state.appRedirectURLstate} onChange={this.appRedirectUrlChanged}  innerRef={(input) => this.appRedirectURL = input} placeholder="Enter appRedirectURL" />
-                                    <FormFeedback invalid={this.state.appRedirectURLstate}>Required</FormFeedback>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row style={{marginBottom:8}}>
-                                <Label sm={1}></Label>
-                                <Label sm={1}>certAlias</Label>
-                                <Col sm={4}>
-                                    <Input type="text" name="certAlias" invalid={this.state.certAliasstate} onChange={this.certAliasChanged}  innerRef={(input) => this.certAlias = input} placeholder="Enter certAlias" />
+                                    <Input type="text" name="certAlias" invalid={this.state.certAliasstate} onChange={this.certAliasChanged}  innerRef={(input) => this.certAlias = input} placeholder="Enter Certificate Alias" />
                                     <FormFeedback invalid={this.state.certAliasstate}>Required</FormFeedback>
                                 </Col>
-                                <Label sm={1}>certPassword</Label>
-                                <Col sm={4}>
-                                    <Input type="text" name="certPassword" invalid={this.state.certPasswordstate} onChange={this.certPasswordChanged}  innerRef={(input) => this.certPassword = input} placeholder="Enter certPassword" />
-                                    <FormFeedback invalid={this.state.certPasswordstate}>Required</FormFeedback>
-                                </Col>
                             </FormGroup>
                             <FormGroup row style={{marginBottom:8}}>
                                 <Label sm={1}></Label>
-                                <Label sm={1}>certText</Label>
+                                <Label sm={1}>Certificate (PEM)</Label>
                                 <Col sm={9}>
-                                    <Input type="textarea" name="certText" invalid={this.state.certTextinvalid} invalid={this.state.certTextstate}  onChange={this.certTextChanged}    innerRef={(input) => this.certText = input} placeholder="Enter certText" />
+                                    <Input type="textarea" name="certText" invalid={this.state.certTextinvalid} invalid={this.state.certTextstate}  onChange={this.certTextChanged}    innerRef={(input) => this.certText = input} placeholder="Enter Certificate (PEM)" />
                                     <FormFeedback invalid={this.state.certTextstate}>{this.state.certErrorLable}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row style={{marginBottom:8}}>
                                 <Label sm={1}></Label>
-                                <Label sm={1}>expireRequestSecs</Label>
+                                <Label sm={1}>Expiration (sec)</Label>
                                 <Col sm={4}>
                                     <Input type="number" name="expireRequestSecs"  innerRef={(input) => this.expireRequestSecs = input}  min={10} max={60}  defaultValue={25}  placeholder="Enter expireRequestSecs" />
                                 </Col>
                                 <Label sm={1}></Label>
                                 <Col sm={2}>
-                                <CustomInput type="switch" innerRef={(input) => this.enabled = input}  id="enabled" onChange={this.enabledChanged} defaultChecked={enabled} name="enabled" label="enable Configuration"/>
+                                <CustomInput type="switch" innerRef={(input) => this.enabled = input}  id="enabled" onChange={this.enabledChanged} defaultChecked={enabled} name="enabled" label="Enable Configuration"/>
                                 </Col>
                             </FormGroup>
                         </Form>
